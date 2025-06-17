3d7a3370bdade685151de219278865be4f040d38
felipepadron <felipe@padronperfumaria.com.br>
2025-06-17 00:04:47 -0300

    Fix: Add all required dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -27,3 +33,4 @@ export default defineConfig({
     host: true
   }
 })
+
